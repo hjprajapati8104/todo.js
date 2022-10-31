7e4d2a0093616e59a8df123ac2704660712cacd9
Happy Prajapati <hjprajapati8104@gmail.com>
2022-10-31 22:36:50 +0530

    update

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 const todoList = () => {
-  all = []
+  const all = [];
+
+  let today = new Date().toLocaleDateString("en-CA");
   const add = (todoItem) => {
-    all.push(todoItem)
-  }
+    all.push(todoItem);
+  };
   const markAsComplete = (index) => {
-    all[index].completed = true
-  }
+    all[index].completed = true;
+  };
 
   const overdue = () => {
-    return all.filter((item) => item.dueDate === yesterday)
-  }
+    return all.filter((item) => item.dueDate < today);
+  };
 
   const dueToday = () => {
-    return all.filter((item) => item.dueDate === today)
-  }
+    return all.filter((item) => item.dueDate === today);
+  };
 
   const dueLater = () => {
-    return all.filter((item) => item.dueDate === tomorrow)
-  }
+    return all.filter((item) => item.dueDate > today);
+  };
 
   const toDisplayableList = (list) => {
-    
-    return list.map(item => `${item.completed ? `[x]`: `[ ]`} ${item.title} ${item.dueDate === yesterday ? `${yesterday}`: (item.dueDate === today ? " " : `${tomorrow}`)}`).join('\n')
-  }
+    return list
+      .map(
+        (item) =>
+          `${item.completed ? `[x]` : `[ ]`} ${item.title} ${
+            item.dueDate != today ? item.dueDate : " "
+          }`
+      )
+      .join("\n");
+  };
 
-  return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
-}
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+  };
+};
 
-
-module.exports = todoList;
+module.exports = todoList;
